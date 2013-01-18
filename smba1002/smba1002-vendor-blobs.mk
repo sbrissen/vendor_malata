9353ea120a423a214d6e9ee1fc0d71ee9a1778d2
jellybean <utahsbraincell@gmail.com>
2013-01-18 03:29:37 -0600

    initial changes to boot cm10.1
    Change-Id: I474b19b59f65a05312d2ede5c6ab5f4bf952dd3d

--- smba1002/smba1002-vendor-blobs.mk
+++ smba1002/smba1002-vendor-blobs.mk
@@ -110,3 +110,5 @@ PRODUCT_COPY_FILES += \
  $(LOCAL_PATH)/proprietary/lib/egl/libGLESv2_tegra.so:system/lib/egl/libGLESv2_tegra.so \
  $(LOCAL_PATH)/proprietary/lib/hw/gralloc.tegra.so:system/lib/hw/gralloc.tegra.so \
  $(LOCAL_PATH)/proprietary/lib/hw/hwcomposer.tegra.so:system/lib/hw/hwcomposer.tegra.so \
+ $(LOCAL_PATH)/proprietary/lib/libwvm.so:system/lib/libwvm.so \
+ $(LOCAL_PATH)/proprietary/lib/libWVStreamControlAPI_L3.so:system/lib/libWVStreamControlAPI_L3.so \
